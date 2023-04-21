cb80a5ac16c5d1485d1b01ed6dae4ac44270869d
Albaryys <a.tily.pro@gmail.com>
2023-04-21 06:48:18 -0700

    utility prototipe

--- utility.c
+++ utility.c
@@ -1,10 +1,1 @@
 #include "simple_shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-/**
- * _getline - read a line from standard input
- *
- * Return: the line read from standard input, or NULL on failure or end of file.
- */
