dbfa768ae4e7d7917a0bc3ec7dcd03967e17d48d
MarcP70 <marcpourias@yahoo.fr>
2023-04-26 02:54:24 -0700

    Modify: declaration of functions prototype

--- simple_shell.h
+++ simple_shell.h
@@ -9,13 +9,13 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-int _strlen(char *s);
-int _strcmp(char *s1, char *s2);
-char *_strdup(const char *str);
-char *_strcat(char *dest, char *src);
-char *prompt(void);
-char *getinput(void);
-char *_getenv(const char *name);
-char *search_path(char *command, char **env);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+char *_getenv(const char *name, char **env);
+size_t _strlen(const char *s);
+int _strncmp(const char* s1, const char* s2, size_t n);
+char *find_program_in_path(char *program_name, char **path_list);
+char **tokenize_str_to_array(char* cmd, char *delimiter);
+void free_array(char **arr);
 
 #endif
